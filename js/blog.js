abb0228dd9408229d65af256b64f335447e07e5e
MD. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-04-11 21:24:46 +0600

    fix blog page switch issue

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 // event listener for home button
 document.getElementById("btn-home").addEventListener("click", function () {
   //   back to home (index.html) page
-  window.location.href = "../index.html";
+  window.location.href = "index.html";
 });
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
  */
 document.getElementById("btn-blog").addEventListener("click", function () {
   //   go to the blog.html page
-  window.location.href = "../blog.html";
+  window.location.href = "blog.html";
 });
 
 /**
